3f5159601e4219ced3f5e715bfbfdb061ac9b8fc
ayswl <aiyas.aboobakar@gmail.com>
2018-10-28 17:35:44 +0800

    add routes, add readme

--- hello.py
+++ hello.py
@@ -2,8 +2,20 @@ from flask import Flask
 app = Flask(__name__)
  
 @app.route("/")
-def hello():
-    return "First Flask App Baby"
+def index():
+    return 'Index'
+
+@app.route("/testing")
+def testing():
+    return 'Testing Route'
+
+@app.route("/profile")
+def profile():
+    return 'Profile'
  
+@app.route("/profile/<string:name>")
+def getProfile(name):
+    return name
+
 if __name__ == "__main__":
     app.run()
---
+++ readme.md
@@ -0,0 +1,3 @@
+this is me trying https://pythonspot.com/flask-web-app-with-python/
+
+trying out flask for a simple app to get that hacktoberfest tshirt :D
