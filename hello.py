b615262e92ff9dfa12de634e123b63d4c56e5cc5
ayswl <aiyas.aboobakar@gmail.com>
2018-11-01 06:18:39 +0800

    adding

--- hello.py
+++ hello.py
@@ -7,5 +7,11 @@ def index():
     name = "You"
     return render_template('index.html', author=author, name=name)
 
+@app.route("/anotherone")
+def this():
+    author = "Me"
+    name = "You"
+    return render_template('this.html', author=author, name=name)
+
 if __name__ == "__main__":
     app.run()
